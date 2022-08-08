d3dab607d5a826f54d5c954fdd54cfcf7cc45724
Yana <jana-ivanova@mail.ru>
2022-08-08 13:37:51 +0400

    Домашняя задача 4

---
+++ Task4/Program.cs
@@ -0,0 +1,37 @@
+﻿/* 
+Домашняя задача 4:
+Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+Console.WriteLine("Привет! Я умею находить максимальное число из трех!");
+Console.WriteLine("Последовательно введи с клавиатуры любые три числа");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+int threeNumber = Convert.ToInt32(Console.ReadLine());
+
+if (firstNumber > secondNumber)
+{
+    if (firstNumber > threeNumber)
+    {
+        Console.WriteLine("Максимальное число = " + firstNumber);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число = " + threeNumber);
+    }
+}
+else
+{
+    if (secondNumber > threeNumber)
+    {
+        Console.WriteLine("Максимальное число = " + secondNumber);
+    }
+    else
+    {
+        Console.WriteLine("Максимальное число = " + threeNumber);
+    }
+}
